b011d765217fea3f60cfbec53abadbf6b3760518
Akshat Virmani <akshatvirmani93@gmail.com>
2022-05-12 12:35:27 +0530

    Added Instructions

--- src/Component/Highlight_2.js
+++ src/Component/Highlight_2.js
@@ -95,6 +95,13 @@ function HighlightTwo() {
       bgSize={"cover"}
     >
       <Navbar color="white" />
+   
+    <Box color="white" rounded={"2xl"} px={"20"} m={20} bg= {"rgb(255,160,122,.9)"}>
+      Instructions:
+      <p class="list" >Edit the text like a normal word document. You can add and remove text too.</p>
+      <p class="list">Click on "Highlight" <BiPencil fontSize={"20px"} color={"cyan"}/>and simply highlight the words you want to blank out.</p>
+      <p class="list">After editing, click on "done" <BiCheck fontSize={"30px"} color={"cyan"}/> to create your fill-in quiz.</p>
+    </Box>
       <Box bg={"white"} m={20} rounded={"2xl"}>
         <HStack
           my={5}
